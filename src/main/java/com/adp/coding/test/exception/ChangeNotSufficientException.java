7d2981ad8f074f06305bc3ae5b9eed3c9b75fd5b
Shahram_Niki <shmniki@hotmail.com>
2022-07-27 16:17:45 -0400

    first commit

--- src/main/java/com/adp/coding/test/exception/ChangeNotSufficientException.java
+++
@@ -1,7 +0,0 @@
-package com.adp.coding.test.exception;
-
-public class ChangeNotSufficientException extends RuntimeException {
-    public ChangeNotSufficientException(Double bill){
-        super("Bill:" + bill + " is more than existing change." );
-    }
-}
